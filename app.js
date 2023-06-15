e34ce85f38a6ca104a15d2e6d8dcc2f888ff290e
saicharanars <saicharan_@outlook.com>
2023-06-15 22:18:18 +0530

    jenkinstestcommit

--- app.js
+++ app.js
@@ -1,43 +1,45 @@
 const path = require("path");
-const fs=require("fs");
-const helmet=require("helmet");
-const morgan=require("morgan");
+const fs = require("fs");
+const helmet = require("helmet");
+const morgan = require("morgan");
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const Sequelize = require("./util/database");
-const { sequelize } = require('./models/expense');
+const { sequelize } = require("./models/expense");
 const homeRoutes = require("./routes/homeRoutes");
-const userRoutes=require("./routes/userRoutes");
-const logStream=fs.createWriteStream(path.join(__dirname,'access.log'),{flags:'a'})
-const User=require("./models/users")
-const Expense =require("./models/expense")
-const Order=require("./models/orders");
-const purchaseRoutes=require("./routes/purchase");
+const userRoutes = require("./routes/userRoutes");
+const logStream = fs.createWriteStream(path.join(__dirname, "access.log"), {
+  flags: "a",
+});
+const User = require("./models/users");
+const Expense = require("./models/expense");
+const Order = require("./models/orders");
+const purchaseRoutes = require("./routes/purchase");
 const errorController = require("./controllers/errorcontroller");
-const Premium= require("./models/premiumuser");
-const forgetPasswordRoutes = require('./routes/forgotpassword');
-const Forgotpassword = require('./models/forgotpassword');
-const DownloadedFile=require('./models/download');
+const Premium = require("./models/premiumuser");
+const forgetPasswordRoutes = require("./routes/forgotpassword");
+const Forgotpassword = require("./models/forgotpassword");
+const DownloadedFile = require("./models/download");
 var cors = require("cors");
 app.use(cors());
-app.use(helmet({
-    contentSecurityPolicy: false
-}));
-
+app.use(
+  helmet({
+    contentSecurityPolicy: false,
+  })
+);
 app.use(bodyParser.json({ extended: false }));
-app.use(morgan('combined',{ stream: logStream }))
-
+app.use(morgan("combined", { stream: logStream }));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(homeRoutes);
 app.use(userRoutes);
-app.use("/purchase/",purchaseRoutes);
-app.use('/password',forgetPasswordRoutes);
-app.use((req,res)=>{
-    console.log('url',req.url);
-    res.sendFile(path.join(__dirname,`public/${req.url}`));
-})
-User.hasMany(Expense, { as: 'expenses' });
+app.use("/purchase/", purchaseRoutes);
+app.use("/password", forgetPasswordRoutes);
+app.use((req, res) => {
+  console.log("url", req.url);
+  res.sendFile(path.join(__dirname, `public/${req.url}`));
+});
+User.hasMany(Expense, { as: "expenses" });
 Expense.belongsTo(User);
 User.hasMany(Order);
 Order.belongsTo(User);
@@ -47,12 +49,6 @@ User.hasMany(Forgotpassword);
 Forgotpassword.belongsTo(User);
 User.hasMany(DownloadedFile);
 DownloadedFile.belongsTo(User);
-
-
-
-
-
-
 
 app.use(errorController.get404);
 
@@ -66,4 +62,4 @@ sequelize
     //console.log(err);
   });
 
-  app.listen(4000);
+app.listen(4000);
